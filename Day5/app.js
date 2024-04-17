a22f052b9de008b1add9202d6c221dc21e354889
jagdeep2393 <jagdeep10790@gmail.com>
2024-04-17 17:53:40 +0530

    Day5 Done

---
+++ Day5/app.js
@@ -0,0 +1,22 @@
+// // To  Show alert Box
+// const MESSAGE="lorum5";
+// alert(MESSAGE);
+// // To show confirm box which takes response from use in the form of OK or cancel button
+// const userChoice=confirm("Are you ready to get your hands dirty with js");
+// if(userChoice)
+// {
+//     alert("Thanks alot");
+// }
+// else{
+//     alert("So sad for you");
+// }
+// // To show prompt box which asking for some input with an optional default value and an input box to input data  
+// let age = prompt('How old are you?', 100);
+// if(age >18)
+// {
+//     alert("Your are a teen ager");
+// }
+// else{
+//     const dif = 18-age;
+//     alert(`sorry kidy try after ${dif}`);
+// }
